f92683e2465b703205cd9298ce670d11be067e84
oraculum <damon.abdiel@gmail.com>
2012-04-14 06:51:43 -0300

    #14 finalizado bug das bordas #10 criado design do atualizar carteira #9 criado design da tabela de rentabilidade

--- public/js/custom.home.js
+++ public/js/custom.home.js
@@ -11,8 +11,47 @@
       $("#year").change(function () {
         pullWallet();
       })
+      
+      $(".update").click(function() {
+		  	var table = document.all.dataTable;
+		  	var addWallet = document.all.addWallet;
+			var rowCount = table.rows.length;
+			
+			for(var i=0; i<rowCount; i++) {
+				var row = table.rows[i];
+				if (i!=0)
+					row.cells[5].style.borderRight = "1px solid #DDD"
+		        row.cells[6].style.display = "none"; 
+		        row.cells[7].style.display = "none"; 
+		        row.cells[8].style.display = "none";               
+			}
+			
+			table.width = "70%";
+			addWallet.style.display = "inline"; 
+			
+			//$("#dataTable thead tr:first th:last").style = "none";
+		});
+		
+	    $(".close").click(function() {
+    		var table = document.all.dataTable;
+		  	var addWallet = document.all.addWallet;
+			var rowCount = table.rows.length;
+			
+			for(var i=0; i<rowCount; i++) {
+				var row = table.rows[i];
+				if (i!=0) 
+					row.cells[5].style.borderRight = "none";
+			    row.cells[6].style.display = ""; 
+			    row.cells[7].style.display = ""; 
+			    row.cells[8].style.display = ""; 
+			}
+			
+			table.width = "100%";
+			addWallet.style.display = "none"; 
+    	});
     });
     
+
     
     
     function pullWallet() {
